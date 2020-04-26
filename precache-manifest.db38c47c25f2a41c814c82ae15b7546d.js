d6ce4237c38663a5f6dac363a9e1672356591449
sparks42 <958575100@qq.com>
2020-04-26 22:07:06 +0800

    deploy

---
+++ precache-manifest.db38c47c25f2a41c814c82ae15b7546d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73db897b5d243d1f1f8c",
+    "url": "/account-book-website/css/app.40e732eb.css"
+  },
+  {
+    "revision": "5fd9a0747745f370cf549a94ff0d8523",
+    "url": "/account-book-website/index.html"
+  },
+  {
+    "revision": "73db897b5d243d1f1f8c",
+    "url": "/account-book-website/js/app.d7601d75.js"
+  },
+  {
+    "revision": "671155e99d2907e822a9",
+    "url": "/account-book-website/js/chunk-vendors.86ef745a.js"
+  },
+  {
+    "revision": "72f5972484b4373630ed6e04cd161071",
+    "url": "/account-book-website/manifest.json"
+  },
+  {
+    "revision": "50fd3d5aa48495bb4fca2d4f41de51dd",
+    "url": "/account-book-website/qrcode.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/account-book-website/robots.txt"
+  }
+]);
